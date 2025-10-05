d1f010d4501f080e1ece2fd03786dbb284620da6
Guilherme <weckwerthguilherme@gmail.com>
2025-10-05 19:04:37 -0300

    Adicao do CORS para permitir Cross Origin Request

--- app/main.py
+++ app/main.py
@@ -2,12 +2,27 @@ from fastapi import FastAPI
 from .routers import links
 from .database import engine
 from . import models
+from fastapi.middleware.cors import CORSMiddleware
 
 # Cria as tabelas no banco de dados (se não existirem)
 # Em um projeto real, isso seria feito com uma ferramenta de migração como Alembic
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Link Aggregator API")
+
+origins = [
+    "http://localhost:5173",  # A URL do seu app React em desenvolvimento rodando localmente
+    # "https://sua-url-do-frontend.vercel.app", # Adicionaremos a URL de produção aqui depois
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"], # Permite todos os cabeçalhos
+)
+
 
 # Inclui as rotas definidas no arquivo de routers
 app.include_router(links.router)
